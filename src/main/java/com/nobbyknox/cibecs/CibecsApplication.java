67794e821e245f7a5f4662f915c06690f5746ae8
Nobby Knox <nobbyknox@gmail.com>
2019-05-02 19:22:55 +0200

    Hello world

---
+++ src/main/java/com/nobbyknox/cibecs/CibecsApplication.java
@@ -0,0 +1,12 @@
+package com.nobbyknox.cibecs;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibecsApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+	}
+
+}
